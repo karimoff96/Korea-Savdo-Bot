7b23b45fb7a062e6851baa4574f27e7febbc35a8
doniyor <doniyorkarimoff96@gmail.com>
2022-02-20 00:52:39 +0500

    first commit

--- Bot/controller.py
+++ Bot/controller.py
@@ -13,16 +13,16 @@ class Controller:
         self.chatid = chatid
         self.m = m
 
-    def cat(self):
-        category = Category.objects.all()
-        ikey = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        l = len(category)
-        print(category, l)
-        for i in range(0, l, 2):
-            btn = types.KeyboardButton(f'{category[i].name}')
-            if l % 2 == 1 and l - 1 == i:
-                ikey.add(btn)
-            else:
-                btn1 = types.KeyboardButton(f'{category[i + 1].name}')
-                ikey.add(btn, btn1)
-            bot.send_message(self.userid, '<b>Kategoriyani tanlang</b>', reply_markup=ikey, parse_mode='HTML')
+    # def cat(self):
+    #     # category = Category.objects.all()
+    #     ikey = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    #     l = len(category)
+    #     print(category, l)
+    #     for i in range(0, l, 2):
+    #         btn = types.KeyboardButton(f'{category[i].name}')
+    #         if l % 2 == 1 and l - 1 == i:
+    #             ikey.add(btn)
+    #         else:
+    #             btn1 = types.KeyboardButton(f'{category[i + 1].name}')
+    #             ikey.add(btn, btn1)
+    #         bot.send_message(self.userid, '<b>Kategoriyani tanlang</b>', reply_markup=ikey, parse_mode='HTML')
